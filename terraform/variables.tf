4010dbe6234e8224a926085074e2ebe23111f3a3
okechukwu-omaliko <okechukwu.omaliko@gmail.com>
2025-02-09 08:52:08 +0100

    changes to files

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -44,7 +44,7 @@ variable "iam-role" {
 variable "ami_id" {
   description = "AMI ID for the EC2 instance"
   type        = string
-  default     = "ami-0c7217cdde317cfec" // Replace with the latest AMI ID for your region
+  default     = "ami-04b4f1a9cf54c11d0" // Replace with the latest AMI ID for your region
 }
 
 variable "instance_type" {
